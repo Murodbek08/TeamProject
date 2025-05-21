b94ae2efe4b216a2bc6020a792a3afb00d59009b
Jahongir-coder-2003 <ravshanovj45@gmail.com>
2025-05-21 13:15:33 +0500

    home page

---
+++ js/data-main.js
@@ -0,0 +1,91 @@
+const products = [
+  {
+    title: "Apple iPhone 14 Pro Max 128GB Deep Purple",
+    price: 900,
+    image: "../images/main/iphone14.png",
+    liked: false,
+  },
+  {
+    title: "Blackmagic Pocket Cinema Camera 6k",
+    price: 2535,
+    image: "../images/main/iphone14-2.png",
+    liked: false,
+  },
+  {
+    title: "Apple Watch Series 9 GPS 41mm Starlight Aluminium",
+    price: 399,
+    image: "../images/main/iphone14-3.png",
+    liked: false,
+  },
+  {
+    title: "AirPods Max Silver Starlight Aluminium",
+    price: 549,
+    image: "../images/main/iphone14-4.png",
+    liked: false,
+  },
+  {
+    title: "Samsung Galaxy Watch6 Classic 47mm Black",
+    price: 369,
+    image: "../images/main/iphone14-5.png",
+    liked: false,
+  },
+  {
+    title: "Galaxy Z Fold5 Unlocked | 256GB | Phantom Black",
+    price: 1799,
+    image: "../images/main/iphone14-6.png",
+    liked: true,
+  },
+  {
+    title: "Galaxy Buds FE Graphite",
+    price: 99.99,
+    image: "../images/main/iphone14-7.png",
+    liked: false,
+  },
+  {
+    title: 'Apple iPad 9 10.2" 64GB Wi-Fi Silver (MK2L3) 2021',
+    price: 398,
+    image: "../images/main/iphone14-8.png",
+    liked: false,
+  },
+];
+
+
+
+
+// 4tali card
+const productData = [
+  {
+    id: 1,
+    title: "Popular Products",
+    description:
+      "Modern smartwatches and earbuds with high-definition displays, health tracking, and stylish design.",
+    image: "../images/main/page5-img1.png",
+    buttonText: "Shop Now",
+  },
+  {
+    id: 2,
+    title: "iPad Pro",
+    description:
+      "A 10.2-inch Retina display, powerful performance, multitasking support, and seamless usability.",
+    image: "../images/main/page5-img2.png",
+    buttonText: "Shop Now",
+  },
+  {
+    id: 3,
+    title: "Samsung Galaxy",
+    description:
+      "Next-gen foldable smartphone with immersive display, advanced cameras, and cutting-edge features.",
+    image: "../images/main/page5-img3.png",
+    buttonText: "Shop Now",
+  },
+  {
+    id: 4,
+    title: "MacBook Pro",
+    description:
+      "Equipped with M1/M2 chips, brilliant Retina display, and powerful performance for professionals.",
+    image: "../images/main/page5-img4.png",
+    buttonText: "Shop Now",
+  },
+];
+
+
